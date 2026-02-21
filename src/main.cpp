0f3f55f84652bd79c1ffdd17a11ddc06d91d6cab
Will Flores <waflores956@gmail.com>
2026-02-21 11:50:50 -0500

    Adding clang formatting and tidying files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
-#include <Arduino.h>
-#include <Wire.h>
-#include <SerLCD.h> //Click here to get the library: http://librarymanager/All#SparkFun_SerLCD
-#include "avr8-stub.h"
 #include "app_api.h" // only needed with flash breakpoints
+#include "avr8-stub.h"
+#include <Arduino.h>
+#include <SerLCD.h> //Click here to get the library: http://librarymanager/All#SparkFun_SerLCD
+#include <Servo.h>
+#include <Wire.h>
+#include <stdio.h>
 
-void setup()
-{
+SerLCD lcd; // Initialize the library with default I2C address 0x72
+
+int potPosition;   // this variable will store the position of the potentiometer
+int servoPosition; // the servo will move to this position
+char tbs[16];
+
+Servo myservo; // create a servo object
+
+void setup() {
   // put your setup code here, to run once:
   debug_init();
   Wire.begin();
@@ -13,18 +22,69 @@ void setup()
   lcd.begin(Wire); // Set up the LCD for I2C communication
 
   lcd.setBacklight(255, 255, 255); // Set backlight to bright white
-  lcd.setContrast(5);              // Set contrast. Lower to 0 for higher contrast.
+  lcd.setContrast(5); // Set contrast. Lower to 0 for higher contrast.
 
-  lcd.clear(); // Clear the display - this moves the cursor to home position as well
+  lcd.clear(); // Clear the display - this moves the cursor to home position as
+               // well
   lcd.print("Hello, World!");
+  myservo.attach(
+      9); // tell the servo object that its servo is plugged into pin 9
+  servoPosition = 0;
 }
 
-void loop()
-{
+void loop() {
   // put your main code here, to run repeatedly:
   // Set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // Print the number of seconds since reset:
-  lcd.println(millis() / 1000);
+  potPosition = ((millis() / 100) % 180);
+  lcd.println(potPosition);
+  // servoPosition = map(potPosition, 0, 180, 20, 160) % 160; // convert the
+  // potentiometer number to a servo position from 20-160 Note: its best to
+  // avoid driving the little SIK servos all the way to 0 or 180 degrees it can
+  // cause the motor to jitter, which is bad for the servo.
+  myservo.write(15); // goes to 15 degrees
+  delay(1000);       // wait for a second
+
+  myservo.write(30); // goes to 30 degrees
+  delay(1000);       // wait for a second.33
+
+  myservo.write(45); // goes to 45 degrees
+  delay(1000);       // wait for a second.33
+
+  myservo.write(60); // goes to 60 degrees
+  delay(1000);       // wait for a second.33
+
+  myservo.write(75); // goes to 75 degrees
+  delay(1000);       // wait for a second.33
+
+  myservo.write(90); // goes to 90 degrees
+  delay(1000);       // wait for a second
+
+  myservo.write(75); // back to 75 degrees
+  delay(1000);       // wait for a second.33
+
+  myservo.write(60); // back to 60 degrees
+  delay(1000);       // wait for a second.33
+
+  myservo.write(45); // back to 45 degrees
+  delay(1000);       // wait for a second.33
+
+  myservo.write(30); // back to 30 degrees
+  delay(1000);       // wait for a second.33
+
+  myservo.write(15); // back to 15 degrees
+  delay(1000);       // wait for a second
+
+  myservo.write(0); // back to 0 degrees
+  delay(1000);      // wait for a second
+  for (int num = 0; num <= 180; num++) {
+    myservo.write(num); // back to 'num' degrees(0 to 180)
+    delay(10);          // control servo speed
+  }
+  for (int num = 180; num >= 0; num--) {
+    myservo.write(num); // back to 'num' degrees(180 to 0)
+    delay(10);          // control servo speed
+  }
 }
